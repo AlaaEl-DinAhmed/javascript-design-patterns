d5db215c6c56dc79dd91da90f58cfbacb26b6b73
Alaa Mohamed <alaa.mohamed@areebgroup.com>
2020-12-12 12:53:56 +0200

    First initialize

---
+++ main.js
@@ -0,0 +1,37 @@
+class Person {
+  constructor(fname, lname, age, job) {
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.job = job;
+  }
+}
+
+class PersonFactory {
+  createPerson(type) {
+    switch (type) {
+      case "developer":
+        return new Person("Mohamed", "Ahmad", 30, "Developer");
+      case "teacher":
+        return new Person("Alaa El-Din", "Ahmad", 29, "Teacher");
+    }
+  }
+}
+
+class Teacher extends Person {
+  constructor(fname, lname, age, job, id) {
+    super(fname, lname, age);
+    this.job = job;
+    this.id = id;
+  }
+}
+
+const personFactory = new PersonFactory();
+
+const developer = personFactory.createPerson("developer");
+
+const teacher = personFactory.createPerson("teacher");
+
+console.log(developer);
+
+console.log(teacher);
